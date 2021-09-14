26b6d5f166ec5684c574018718bd2f67399ad7a5
Pedro Mateus <pedro.mateus@maastro.nl>
2021-09-14 11:50:02 +0200

    Improve histogram

--- v6_summary_rdb/sql_functions.py
+++ v6_summary_rdb/sql_functions.py
@@ -4,6 +4,13 @@ def histogram(variable, table, arguments):
     """ Create the SQL statement to obtain the necessary information
         for an histogram.
     """
-    width = arguments[BIN_WIDTH]
-    return f"""SELECT floor("{variable}"*{width})/{width} as bins, COUNT(*) 
+    width = None
+    if BIN_WIDTH in arguments:
+        width = arguments[BIN_WIDTH]
+        if not isinstance(width, int) or width <= 1:
+            raise Exception("Invalid bin width provided, value must be a integer superior to 1.")
+    else:
+        raise Exception("Histogram requested but the bin width (argument: BIN_WIDTH) must be provided!")
+
+    return f"""SELECT floor("{variable}"/{width})*{width} as bins, COUNT(*) 
         FROM {table} GROUP BY 1 ORDER BY 1"""
